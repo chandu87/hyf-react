7d6552524e6d6e7b4e8bd7d23fe9dd98d5d5cd00
chandu <chandrasr.palle@gmail.com>
2018-09-11 16:50:16 +0200

    fetchApi added

---
+++ week4_hw/src/api.js
@@ -0,0 +1,8 @@
+export function fetchGithub(searchKeyword){
+    const url = `https://api.github.com/search/users?q=${searchKeyword}`;
+    return fetch(url).then(response=>response.json());
+};
+export function fetchGithubUser(userName){
+    const url = `https://api.github.com/users/${userName}`;
+    return fetch(url).then(response => response.json());
+};
