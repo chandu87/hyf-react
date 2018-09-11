9bddc63a07c3cbab45cd28605c9083d775dda4ba
chandu <chandrasr.palle@gmail.com>
2018-09-11 16:50:33 +0200

    component modified

--- week4_hw/src/components/GithubSearch.js
+++ week4_hw/src/components/GithubSearch.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {fetchGithub, fetchGithubUser} from '../api';
 
 
 class GithubSearch extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { searchKeyword: "", githubUserData: [] };
+        this.search = this.search.bind(this);
+      }
+    search(){
+        console.log("serach function invoked");
+        // fetchGithub("chandu").then((data)=>{
+        //     console.log(data);
+        // });
+        fetchGithubUser("chandu87").then((data)=>{
+            this.setState({githubUserData : data});
+            console.log(this.state.githubUserData);
+        });
+    }
     render(){
-        return "Hello GithubSearch";
+        return (
+        <div>
+            <button onClick={this.search}>Submit</button>
+            <p>{this.state.githubUserData.login}</p>
+            <p>{this.state.githubUserData.location}</p>
+
+        </div>
+        );
     }
 }
 
