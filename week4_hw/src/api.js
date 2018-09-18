4c10c07f8f3956b21146e2b841069989fda0e39a
chandu <chandrasr.palle@gmail.com>
2018-09-18 14:15:22 +0200

    fetch api modified

--- week4_hw/src/api.js
+++ week4_hw/src/api.js
@@ -6,3 +6,7 @@ export function fetchGithubUser(userName){
     const url = `https://api.github.com/users/${userName}`;
     return fetch(url).then(response => response.json());
 };
+export function fetchHyfRepos(){
+    const url = `https://api.github.com/users/HackYourFuture-CPH/repos`;
+    return fetch(url).then(response => response.json());
+}
